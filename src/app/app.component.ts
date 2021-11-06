78b65aea0c7fdf99993af26c67da3c2bff5be31a
José Espinoza Salas <espinozasalas.jose@gmail.com>
2021-11-06 12:23:44 -0300

    Se añade meta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,20 @@ export class AppComponent implements OnInit {
 
   title: string = 'Hola!';
 
+  constructor(
+    private meta: Meta
+  ) {
+  }
+
   ngOnInit() {
+    this.meta.addTags([
+      { name: 'keywords', content: 'Angular SEO Integration, Music CRUD, Angular Universal' },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'author', content: 'Curso Angular' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'date', content: '2021-11-06', scheme: 'YYYY-MM-DD' },
+      { charset: 'UTF-8' }
+    ]);
   }
 
 }
